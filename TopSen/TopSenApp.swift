1850fa17ed2cfa5686c2707dd1b043cd770a09c6
kyo kasa <kassaaaah@gmail.com>
2026-08-06 22:02:07 +0900

    Initial Commit

---
+++ TopSen/TopSenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopSenApp.swift
+//  TopSen
+//
+//  Created by 笠原恭平 on 2026/08/06.
+//
+
+import SwiftUI
+
+@main
+struct TopSenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TopSenTests/TopSenTests.swift
@@ -0,0 +1,17 @@
+//
+//  TopSenTests.swift
+//  TopSenTests
+//
+//  Created by 笠原恭平 on 2026/08/06.
+//
+
+import Testing
+@testable import TopSen
+
+struct TopSenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
